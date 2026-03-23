852fd2bd7f68f6a5f245906a16cb641601819b29
Trapti <traptivishwakarma2007@gmail.com>
2026-03-23 23:14:10 +0530

    added auth system

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,25 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const connectToDb = require('./db/db');
+const cookieParser = require('cookie-parser');
+const userRoutes = require("./routes/user.routes")
 
-app.use(cors());
+connectToDb();
+
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
 
 
 app.get('/', (req, res)=>{
     res.send("Hello World");
 })
+
+app.use('/users', userRoutes);
 
 module.exports = app;
